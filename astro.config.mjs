950c7f547e7db092821a42571c37a439c80026e6
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-10-21 22:49:18 +0900

    chore: main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Growth Diary",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/jingjing2222/growth-diary",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Engineering",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "목적 지향 사고 탈피하기", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Others",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
